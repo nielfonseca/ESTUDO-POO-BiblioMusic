6cc1e5c2e73c8b8d2ed9675053dba30862522ea0
nielfonseca <daniel97rf@gmail.com>
2025-06-10 23:25:34 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto2/Program.cs
@@ -0,0 +1,15 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Radioactive";
+musica1.artista = "Imagine Dragons";
+musica1.duracao = 305;
+musica1.disponivel = true;
+
+
+Musica musica2 = new Musica();
+musica2.nome = "505";
+musica2.artista = "Arctic Monkeys";
+musica2.duracao = 408;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
