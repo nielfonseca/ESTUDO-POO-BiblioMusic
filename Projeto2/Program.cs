32dc995de940c1d7ea090732e9abfc099282bd5d
nielfonseca <daniel97rf@gmail.com>
2025-06-11 23:14:14 -0300

    Aprendendo conceitos de get e set (propriedades), lambda, ctrl + R + ctrl + R, para alterar o nome de todas as referências no programa.

--- Projeto2/Program.cs
+++ Projeto2/Program.cs
@@ -1,15 +1,17 @@
 ﻿Musica musica1 = new Musica();
-musica1.nome = "Radioactive";
-musica1.artista = "Imagine Dragons";
-musica1.duracao = 305;
-musica1.disponivel = true;
+musica1.Nome = "Radioactive";
+musica1.Artista = "Imagine Dragons";
+musica1.Duracao = 305;
+musica1.Disponivel = true;
+
 
 
 Musica musica2 = new Musica();
-musica2.nome = "505";
-musica2.artista = "Arctic Monkeys";
-musica2.duracao = 408;
-musica2.disponivel = false;
+musica2.Nome = "505";
+musica2.Artista = "Arctic Monkeys";
+musica2.Duracao = 408;
+musica2.Disponivel = false;
 
 musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
+
